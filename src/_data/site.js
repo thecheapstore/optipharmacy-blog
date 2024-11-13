209bc13cb20287f10475ff1ec110e281b354a6c2
thecheapstore <jackwinchester943@gmail.com>
2024-11-13 05:32:36 +0530

    Basic Changes

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: 'Vredeburg',
-  description: 'Eleventy starter project to create a blog',
-  keywords: ['eleventy', 'template', 'simple', 'clean'],
-  url: 'https://vredeburg.netlify.app', // your site url without trailing slash
-  paginate: 6 // how many posts you want to show for each page
+  title: 'Opti Pharmacy Blog',
+  description: 'Your source for reliable information on medicine, health tips, and pharmaceutical insights.',
+  keywords: ['opti pharmacy', 'medicine reviews', 'health blog', 'pharmaceuticals', 'online pharmacy', 'health tips'],
+  url: 'https://optipharmacyblog.netlify.app', // replace with your actual blog domain without trailing slash
+  paginate: 6, // adjust if you'd like a different number of posts per page
   // uncomment the next line if you want to add disqus to your site
-  // disqusShortname: "your-shortname"
+  // disqusShortname: "your-disqus-shortname"
 };
